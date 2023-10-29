e3d4f3d3ac7ada310d5283f7d8f905b246b07cd7
MohamedGoush <0x1110111@gmail.com>
2023-10-29 15:01:47 +0530

    Initial Commit

---
+++ Animating Circles/Animating_CirclesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animating_CirclesApp.swift
+//  Animating Circles
+//
+//  Created by python on 29/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Animating_CirclesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

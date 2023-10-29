df7dc59cda3c1822036b03ef4696df0f8a4e2bec
MohamedGoush <0x1110111@gmail.com>
2023-10-29 15:25:00 +0530

    Animation Circles with Breathing Animation

--- Animating Circles/ContentView.swift
+++ Animating Circles/ContentView.swift
@@ -8,14 +8,72 @@
 import SwiftUI
 
 struct ContentView: View {
+    //MARK: Properties
+    
+    @State private var scaleInOut = false
+    @State private var rotateInOut = false
+    @State private var moveInOut = false
+    
+    //MARK: BODY
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        ZStack{
+            Rectangle()
+                .edgesIgnoringSafeArea(.all)
+            ZStack{
+                
+                //MARK: Circleset 1
+                ZStack{
+                    Circle()
+                        .fill(LinearGradient(colors: [.green, .white], startPoint: .top, endPoint:  .bottom))
+                        .frame(width: 120, height: 120)
+                        .offset(y: moveInOut ? -60 : 0)
+                    
+                    Circle()
+                        .fill(LinearGradient(colors: [.green, .white], startPoint: .top, endPoint:  .bottom))
+                        .frame(width: 120, height: 120)
+                        .offset(y: moveInOut ? 60 : 0)
+                }.opacity(0.5)
+                
+                //MARK: Circleset 2
+                ZStack{
+                    Circle()
+                        .fill(LinearGradient(colors: [.green, .white], startPoint: .top, endPoint:  .bottom))
+                        .frame(width: 120, height: 120)
+                        .offset(y: moveInOut ? -60 : 0)
+                    
+                    Circle()
+                        .fill(LinearGradient(colors: [.green, .white], startPoint: .top, endPoint:  .bottom))
+                        .frame(width: 120, height: 120)
+                        .offset(y: moveInOut ? 60 : 0)
+                }.opacity(0.5)
+                    .rotationEffect(.degrees(60))
+                
+                //MARK: Circleset 3
+                ZStack{
+                    Circle()
+                        .fill(LinearGradient(colors: [.green, .white], startPoint: .top, endPoint:  .bottom))
+                        .frame(width: 120, height: 120)
+                        .offset(y: moveInOut ? -60 : 0)
+                    
+                    Circle()
+                        .fill(LinearGradient(colors: [.green, .white], startPoint: .top, endPoint:  .bottom))
+                        .frame(width: 120, height: 120)
+                        .offset(y: moveInOut ? 60 : 0)
+                }.opacity(0.5)
+                    .rotationEffect(.degrees(120))
+                
+                
+            }.rotationEffect(.degrees(rotateInOut ? 90 : 0))
+                .scaleEffect(scaleInOut ? 1 : 1/4)
+                .animation(Animation
+                    .easeInOut
+                    .repeatForever(autoreverses: true).speed(1/8 ))
+                .onAppear{
+                    moveInOut.toggle()
+                    scaleInOut.toggle()
+                    rotateInOut.toggle()
+                }
+        }//:BODY
     }
 }
 
